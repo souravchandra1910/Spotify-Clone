aa7ec671de822a7f12ee78d8770fcd9a42cc9e39
souravchandra1910 <souravc399@gmail.com>
2023-01-18 20:33:12 +0530

    v4

--- index.js
+++ index.js
@@ -1,56 +1,62 @@
 console.log("Welcome to Spotify!");
 let songIndex = 0;
-let audioElement = new Audio("./songs/Phir Kabhi.mp3");
+let audioElement = new Audio("./songs/1.mp3");
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressbar");
 let gif = document.getElementById("gif");
+let masterSongName = document.getElementById("masterSongName");
+let songItems = Array.from(document.getElementsByClassName("songItem"));
 let songs = [
   {
     songName: "Phir Kabhi",
-    filepath: "./songs/Phir Kabhi",
+    filepath: "./songs/1",
     coverPath: "./image/c.jpg",
   },
   {
-    songName: "Phir Kabhi",
-    filepath: "./songs/Phir Kabhi",
+    songName: "Dil Ka Pata",
+    filepath: "./songs/2",
     coverPath: "./image/c.jpg",
   },
   {
-    songName: "Phir Kabhi",
-    filepath: "./songs/Phir Kabhi",
+    songName: "Dil Se Dil",
+    filepath: "./songs/3",
     coverPath: "./image/c.jpg",
   },
   {
-    songName: "Phir Kabhi",
-    filepath: "./songs/Phir Kabhi",
+    songName: "Ghodey Pe Sawar",
+    filepath: "./songs/4",
     coverPath: "./image/c.jpg",
   },
   {
-    songName: "Phir Kabhi",
-    filepath: "./songs/Phir Kabhi",
+    songName: "Jaan Ban Gaye",
+    filepath: "./songs/5",
     coverPath: "./image/c.jpg",
   },
   {
-    songName: "Phir Kabhi",
-    filepath: "./songs/Phir Kabhi",
+    songName: "Kaise Hua",
+    filepath: "./songs/6",
     coverPath: "./image/c.jpg",
   },
   {
-    songName: "Phir Kabhi",
-    filepath: "./songs/Phir Kabhi",
+    songName: "Tera Ban Jayunga",
+    filepath: "./songs/7",
     coverPath: "./image/c.jpg",
   },
   {
-    songName: "Phir Kabhi",
-    filepath: "./songs/Phir Kabhi",
+    songName: "Jab Tak",
+    filepath: "./songs/8",
     coverPath: "./image/c.jpg",
   },
   {
-    songName: "Phir Kabhi",
-    filepath: "./songs/Phir Kabhi",
+    songName: "Tuta Hua Saaz",
+    filepath: "./songs/9",
     coverPath: "./image/c.jpg",
   },
 ];
+
+songItems.forEach((element, i) => {
+  element.getElementsByClassName("SongName")[0].innerText=songs[i].songName;
+});
 
 masterPlay.addEventListener("click", () => {
   if (audioElement.paused || audioElement.currentTime <= 0) {
@@ -74,7 +80,63 @@ window.onload = function () {
     myProgressBar.value = progress;
   });
 
-  myProgressBar.addEventListener('change',()=>{
-    audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
-  })
+  myProgressBar.addEventListener("change", () => {
+    audioElement.currentTime =
+      (myProgressBar.value * audioElement.duration) / 100;
+  });
 };
+
+const makaAllPlays = () => {
+  Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+    (element) => {
+      element.addEventListener("click", (e) => {
+        e.target.classList.remove("fa-pause-circle");
+        e.target.classList.add("fa-play-circle");
+      });
+    }
+  );
+};
+Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+  (element) => {
+    element.addEventListener("click", (e) => {
+      makaAllPlays();
+      songIndex=parseInt(e.target.id);
+      e.target.classList.remove("fa-play-circle");
+      e.target.classList.add("fa-pause-circle");
+      audioElement.src=`songs/${songIndex+1}.mp3`; 
+      masterSongName.innerText=songs[songIndex].songName;
+      audioElement.currentTime=0;
+      audioElement.play();
+      masterPlay.classList.remove('fa-play-circle');
+      masterPlay.classList.add('fa-pause-circle');
+    });
+  }
+);
+
+document.getElementById('next').addEventListener('click',()=>{
+  if(songIndex>=8){
+    songIndex=0;
+  }else{
+    songIndex+=1;
+  }
+  audioElement.src=`songs/${songIndex+1}.mp3`;
+  masterSongName.innerText=songs[songIndex].songName; 
+      audioElement.currentTime=0;
+      audioElement.play();
+      masterPlay.classList.remove('fa-play-circle');
+      masterPlay.classList.add('fa-pause-circle');
+    });
+    document.getElementById('previous').addEventListener('click',()=>{
+      if(songIndex<=0){
+        songIndex=0;
+      }else{
+        songIndex-=1;
+      }
+      audioElement.src=`songs/${songIndex+1}.mp3`; 
+          masterSongName.innerText=songs[songIndex].songName;
+          audioElement.currentTime=0;
+          audioElement.play();
+
+          masterPlay.classList.remove('fa-play-circle');
+          masterPlay.classList.add('fa-pause-circle');
+        });
